48b49f6529e878f7f0c84fcf09086c4ec4715f1a
thihaagset01 <thihaagset@gmail.com>
2023-11-20 22:43:10 +0800

    lmaooooooooo

--- Class.py
+++ Class.py
@@ -9,8 +9,10 @@ pen.speed(0)
 pen.hideturtle()
 
 
+# Sprite superclass
 class Sprite:
     def __init__(self):
+        #Sprite superclass attributes
         self.x = 0
         self.y = 0
         self.heading = 0
@@ -22,12 +24,14 @@ class Sprite:
         self.active = True
         
     def update(self):
+        #Sprite superclass animation update  
         if self.active:
             self.x += self.dx
             self.y += self.dy
 
         
     def render(self, pen):
+        #Sprite superclass render sprite
         if self.active:
             pen.goto(self.x, self.y)
             pen.setheading(self.heading)
@@ -36,44 +40,39 @@ class Sprite:
             pen.color(self.color)
             pen.stamp()
             
-    # def is_collision(self, other):
-    #     x = self.x-other.x
-    #     y = self.y-other.y
-    #     distance = ((x**2) + (y**2)) ** 0.5
-    #     if distance < ((10 * self.size) + (10 * other.size)):
-    #         return True
-    #     else:
-    #         return False
+    def is_collision(self, other):
+        #Sprite superclass collision check
+        x = self.x-other.x
+        y = self.y-other.y
+        distance = ((x**2) + (y**2)) ** 0.5
+        if distance < ((10 * self.size) + (10 * other.size)):
+            return True
+        else:
+            return False
             
     def goto(self, x, y):
+        #Sprite location set
         self.x = x
         self.y = y
 
 
+#Player subclass of Sprite
 class Player(Sprite):
     def __init__(self):
+        #Player attributes
         Sprite.__init__(self)
         self.lives = 3
         self.score = 0
         self.x = 0
-        self.y = -HEIGHT/2
+        self.y = -HEIGHT/2+50
         self.heading = 90
-        self.size=2.5
+        self.size=2
         self.shape = "triangle"
         self.color = "yellow"
-    #def rotate_left(self):
-    #    self.heading += 30
-        
-    #def rotate_right(self):
-    #    self.heading -= 30
-        
-    def accelerate(self):
-        ax = math.cos(math.radians(self.heading))
-        ay = math.sin(math.radians(self.heading))
-        self.dx += ax * 0.1
-        self.dy += ay * 0.1
-        
+
+            
     def render(self, pen):
+        #Player render 
         if self.active:
             pen.goto(self.x, self.y)
             pen.setheading(self.heading)
@@ -82,9 +81,10 @@ class Player(Sprite):
             pen.color(self.color)
             pen.stamp()
 
-
+#Ship subclass of Sprite
 class Ship(Sprite):
     def __init__(self):
+        #Ship attributes
         Sprite.__init__(self)
         self.x = 0
         self.y = -300
@@ -92,31 +92,21 @@ class Ship(Sprite):
         self.shape = "square"
         self.color = "gray"
 
+
+#Asteroid subclass of Sprite
 class Asteroid(Sprite):
     def __init__(self):
+        #Asteroid attributes
         Sprite.__init__(self)
         self.shape = "circle"
         self.color= "gray"
         self.word=""
 
-# class Words(Sprite):
-#     def __init__(self):
-#         Sprite.__init__(self)
-#         #self.shape = "circle"
-#         #self.color= "gray"   
-#         # 
-#     def render(self, pen):
-#         if self.active:
-#             pen.goto(self.x, self.y)
-#             pen.write(f"test", False, font=("Courier New", 18, "normal"))
-#             #pen.setheading(self.heading)
-#             #pen.shape(self.shape)
-#             #pen.shapesize(self.size/2.0, self.size, 0)
-#             #pen.color(self.color)
-#             #pen.stamp()     
 
+#Missile subclass of Sprite
 class Missile(Sprite):
     def __init__(self):
+        #Missile attributes
         Sprite.__init__(self)
         self.shape = "square"
         self.color="orange"
@@ -125,27 +115,45 @@ class Missile(Sprite):
         
 
     def update(self):
+        #Missile animation update
         if self.active:
             self.x += self.dx
             self.y += self.dy
             
-            # if self.x > WIDTH/2:
-            #     self.active = False
-            # elif self.x < -(WIDTH/2):
-            #     self.active = False
+            #if out of bounds, missile will despawn
+            if self.x > WIDTH/2:
+                self.active = False
+            elif self.x < -(WIDTH/2):
+                self.active = False
                 
             if self.y > HEIGHT/2:
                 self.active = False
             elif self.y < -(HEIGHT/2):
                 self.active = False
         
-    def fire(self):
+    def fire(self, asteroid):
+        #Missile firing function
         print("FIRE")
         if not self.active:
+            tta=0 #angle
             self.active = True
             self.x = 0
-            self.y = -HEIGHT/2
+            self.y = -HEIGHT/2+50
             self.heading = 90
-            self.dx = math.cos(math.radians(self.heading)) * 1
-            self.dy = math.sin(math.radians(self.heading)) * 1
+            deltay = asteroid.y - self.y
+            deltax = asteroid.x - self.x
+
+            if deltax == 0:
+                tta= math.pi()
+            else:
+                tta= math.atan2(deltay, deltax) #get angle of asteroid wrt missile start pos 
+
+            self.dx = math.cos(tta) *15
+            self.dy = math.sin(tta) *15
+
+        #Missile despawn if collide with asteroid
+        if (self.is_collision(asteroid)):
+            self.active=False
+
+
 
